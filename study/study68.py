cf7b3c9c2f7b38123461e7973c2589d65062c8a6
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-04-14 11:27:36 +0900

    Add files via upload

---
+++ study/study68.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=' ')
+            visited.add(vertex)
+            queue.extend(graph[vertex])
+
+if __name__ == "__main__":
+    # 그래프 정의 (인접 리스트)
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['A', 'D', 'E'],
+        'C': ['A', 'F'],
+        'D': ['B'],
+        'E': ['B', 'F'],
+        'F': ['C', 'E']
+    }
+
+    start_vertex = 'A'
+    print("너비 우선 탐색 결과:")
+    bfs(graph, start_vertex)
