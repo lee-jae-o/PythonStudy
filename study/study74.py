51569025ac4a50cad0e37bc89ff600e7f298d61f
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-04-22 19:27:27 +0900

    Add files via upload

---
+++ study/study74.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+if __name__ == "__main__":
+    arr = [64, 25, 12, 22, 11]
+    insertion_sort(arr)
+    print("정렬된 배열:", arr)
