25cbe7b3824f54d76eddf844e13dbd0a855847eb
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-04-24 20:27:51 +0900

    Delete study/study75.py

--- study/study75.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-if __name__ == "__main__":
-    n = 10
-    print(f"{n}번째 피보나치 수:", fibonacci(n))
